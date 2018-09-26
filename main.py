68b9182dd66a7489e429d665d40e4e9c505170e5
Adam Sawyer <aesawyer@live.com>
2018-09-25 20:37:11 -0600

    Added image files

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from tkinter import ttk
 from tkinter import *
 from tkinter.messagebox import showinfo
+
 import time
 
 def helpPopUp():
-    showinfo('Contact', 'Source Code: \n')
+    showinfo('About', 'Source Code:\n'
+                        'https://github.com/aesawyer/ProjectTimeTracker\n\n'
+                        'Version\n'
+                        '0.1')
 
 root = Tk()
-
-root.title("Time-Productivity Tracker v.0")
+root.title("Project Time Tracker")
+canvas = Canvas(root, width=800, height=400)
+root.geometry('800x400')
 menus = Menu(root)
 filemenu = Menu(menus, tearoff=0)
 helpmenu = Menu(menus, tearoff=0)
@@ -20,8 +25,11 @@ filemenu.add_command(label='Export Report')
 filemenu.add_separator()
 filemenu.add_command(label='Reset')
 filemenu.add_command(label='Quit', command=root.quit)
-helpmenu.add_command(label='Contact', command=helpPopUp)
+helpmenu.add_command(label='About', command=helpPopUp)
 
-timer = ttk.Button(text='Track').grid()
+c1 = PhotoImage(file='img/c1out.pgm')
+canvas.create_image(200,200, anchor=NW, image=c1)
+timer = ttk.Button(text='start').grid()
+
 root.config(menu=menus)
 root.mainloop()
