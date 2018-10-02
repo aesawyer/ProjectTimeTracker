fa8bd2399e82f6eb939834bf229c2f79cb2982ec
Adam Sawyer <aesawyer@live.com>
2018-10-02 12:08:16 -0600

    Restructed code into OOP architecture

--- main.py
+++ main.py
@@ -1,109 +1,107 @@
 from tkinter import ttk
 from tkinter import *
 from tkinter.messagebox import showinfo
-import threading
 import time
 import datetime
 
-def helpPopUp():
-    showinfo('About', 'Source Code:\n'
-                      'https://github.com/aesawyer/ProjectTimeTracker\n\n'
-                      'Version\n'
-                      '0.1')
-
-def stateChange(circle, timer, fill, timerText, button, active):
-    if button.get() == 'Stop':
-        button.set('Start')
-        active = False
-        timerStart(timer, timerText, active)
-        circleChange(circle, fill)
-        return
-    else:
-        button.set('Stop')
-        active = True
-        timer = 0
-        thread = threading.Thread(target=timerStart, args=(timer, timerText, active))
-        thread.daemon = True
-        thread.start()
-        circleChange(circle, fill)
-        return
-
-def timerStart(timer, timerText, active):
-    while active:
-        time.sleep(1)
-        timer += 1
-        print(timer)
-
-def timerStop(timer, timerText):
-    print('timerStop')
-
+'''
 def circleChange(circle, fill):
     if canvas.itemcget(circle, 'fill') == '':
         canvas.itemconfig(circle, fill=fill)
     else:
         canvas.itemconfig(circle, fill='')
+'''
 
-#Base Appearance-----------------------------------------------------
-root = Tk()
-root.title("Project Time Tracker")
-root.resizable(width=False, height=False)
-root.geometry('800x400')
-canvas = Canvas(root, width=800, height=400, bg='gray25')
-canvas.pack()
-canvas.create_rectangle(100, 350, 850, 450, outline='', fill='#ff6600')
-canvas.create_oval(50, 350, 150, 500, outline='', fill='#ff6600')
+class Timer:
+    def __init__(self, root, canvas, count):
+        self.root = root
+        self.canvas = canvas
+        self.count = count
+        #self.x
+        #self.y
+        self.timer = 0
+        self.active = False
+        self.widgets(root, canvas, count)
 
-#File Menus-----------------------------------------------------
-menus = Menu(root)
-filemenu = Menu(menus, tearoff=0)
-helpmenu = Menu(menus, tearoff=0)
+    def widgets(self, root, canvas, count):
+        if count == 1:
+            circnt = 0
+            btncnt = 144 #middle of first circle
+            fill = '#FF1D25'
+        elif count == 2:
+            circnt = 267 #difference in space between top-x of circles
+            btncnt = 61+circnt+83 #61=top-x of circle, 83 is radius of circle
+            fill = '#3FA9F5'
+        elif count == 3:
+            circnt = 534
+            btncnt = 61+circnt+83
+            fill = '#7AC943'
+        title = Entry(root, width=26, justify=CENTER)
+        title.place(x=61+circnt, y=50, anchor=W)
+        clbtn_txt = StringVar()
+        #clbtn = ttk.Button(textvariable=clbtn_txt, command=self.stateChange)
+        clbtn = ttk.Button(text='Start', command=self.stateChange)
+        clbtn_txt.set('Start')
+        timerWindow = canvas.create_window(btncnt, 300, anchor=CENTER, window=clbtn)
+        circle = canvas.create_oval(61+circnt, 90, (61+circnt)+166, 256, width=10, outline=fill)
+        timerText = canvas.create_text(btncnt, 173, anchor=CENTER, fill='white', font=('Consolas', 18), text='0:00:00')
 
-menus.add_cascade(label='File', menu=filemenu)
-menus.add_cascade(label='Help', menu=helpmenu)
+    def stateChange(self):
+        if self.clbtn_txt.get() == 'Stop':
+            self.clbtn_txt.set('Start')
+            self.active = False
+            #self.timerStart()
+            #self.circleChange()
+        else:
+            self.clbtn_txt.set('Stop')
+            self.timer = 0
+            self.active = True
+            # timerStart(timer, timerText, active)
+            #self.circleChange()
 
-filemenu.add_command(label='Export Report')
-filemenu.add_separator()
-filemenu.add_command(label='Reset')
-filemenu.add_command(label='Quit', command=root.quit)
-helpmenu.add_command(label='About', command=helpPopUp)
+class UI:
+    #Base Appearance-----------------------------------------------------
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Project Time Tracker")
+        self.root.resizable(width=False, height=False)
+        self.root.geometry('800x400')
+        self.canvas = Canvas(self.root, width=820, height=420, bg='gray25')
+        self.canvas.place(x=-5, y=-5)
+        self.canvas.create_rectangle(100, 350, 850, 450, outline='', fill='#ff6600')
+        #self.canvas.create_rectangle(100, 100, 200, 200, outline='', fill='#fff000')
+        self.canvas.create_oval(50, 350, 150, 500, outline='', fill='#ff6600')
 
-#Timers-----------------------------------------------------
-c1timer = 0
-c1Active = False
-c1btn_txt = StringVar()
-c1btn = ttk.Button(textvariable=c1btn_txt, command=lambda: stateChange(c1, c1timer, '#FF1D25', c1TimerText, c1btn_txt, c1Active))
-c1btn_txt.set('Start')
-timer1Window = canvas.create_window(133, 300, anchor=CENTER, window=c1btn)
-c1 = canvas.create_oval(50, 90, 216, 256, width=10, outline='#FF1D25', fill='')
-c1TimerText = canvas.create_text(133, 173, anchor=CENTER, fill='white', font=('Consolas', 18), text='0:00:00')
-c1Title = Entry(root, justify=CENTER)
-c1Title.place(x=133, y=50, anchor=CENTER)
+        #File Menus-----------------------------------------------------
+        self.menus = Menu(self.root)
+        self.filemenu = Menu(self.menus, tearoff=0)
+        self.helpmenu = Menu(self.menus, tearoff=0)
 
-c2timer = 0
-c2Active = False
-c2btn_txt = StringVar()
-c2btn = ttk.Button(textvariable=c2btn_txt, command=lambda: stateChange(c2, c2timer, '#3FA9F5', c2TimerText, c2btn_txt, c2Active))
-c2btn_txt.set('Start')
-timer2Window = canvas.create_window(400, 300, anchor=CENTER, window=c2btn)
-c2 = canvas.create_oval(317, 90, 483, 256, width=10, outline='#3FA9F5')
-c2TimerText = canvas.create_text(400, 173, anchor=CENTER, fill='white', font=('Consolas', 18), text='0:00:00')
-c2Title = Entry(root, justify=CENTER)
-c2Title.place(x=400, y=50, anchor=CENTER)
+        self.menus.add_cascade(label='File', menu=self.filemenu)
+        self.menus.add_cascade(label='Help', menu=self.helpmenu)
 
-c3timer = 0
-c3Active = False
-c3btn_txt = StringVar()
-c3btn = ttk.Button(textvariable=c3btn_txt, command=lambda: stateChange(c3, c3timer, '#7AC943', c3TimerText, c3btn_txt, c3Active))
-c3btn_txt.set('Start')
-timer3Window = canvas.create_window(667, 300, anchor=CENTER, window=c3btn)
-c3 = canvas.create_oval(584, 90, 750, 256, width=10, outline='#7AC943')
-c3TimerText = canvas.create_text(667, 173, anchor=CENTER, fill='white', font=('Consolas', 18), text='0:00:00')
-c3Title = Entry(root, justify=CENTER)
-c3Title.place(x=667, y=50, anchor=CENTER)
+        self.filemenu.add_command(label='Export Report')
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label='Reset')
+        self.filemenu.add_command(label='Quit', command=self.quit)
+        self.helpmenu.add_command(label='About', command=self.helpPopUp)
+        self.root.config(menu=self.menus)
 
+        for t in range(3):
+            Timer(self.root, self.canvas, t+1)
 
-#thread = threading.Thread(target=timerStart, args=(timer, timerText))
-#thread.daemon = True
-#thread.start()
-root.config(menu=menus)
-root.mainloop()
+    @staticmethod
+    def helpPopUp():
+        showinfo('About', 'Source Code:\n'
+                          'https://github.com/aesawyer/ProjectTimeTracker\n\n'
+                          'Version\n'
+                          '0.1')
+
+    @staticmethod
+    def quit():
+        app.root.quit()
+        sys.exit()
+
+if __name__ == '__main__':
+    app = UI()
+    app.root.mainloop()
